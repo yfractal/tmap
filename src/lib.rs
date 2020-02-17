ae0bb580aab8b682994bcceb27a495b7f954329e
yfractal <yfractal@gmail.com>
2020-02-17 09:24:45 +0800

    feat: basic single partion implemention

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,9 @@ use tokio::prelude::*;
 use tokio::sync::{oneshot, mpsc};
 use std::collections::HashMap;
 
+// TODO: use tokio's lock for avoiding suspend scheduler
+use std::sync::RwLock;
+
 pub enum Command {
     Read,
     Write
@@ -17,15 +20,7 @@ type Receiver = mpsc::Receiver<Channel>;
 type Sender = mpsc::Sender<Channel>;
 
 pub struct TMap {
-    name: String,
-}
-
-impl TMap {
-    pub fn new(name: String) -> TMap {
-        TMap {
-            name: name
-        }
-    }
+    tables: HashMap<String, Sender>,
 }
 
 pub async fn receive(mut receiver: Receiver) {
@@ -36,9 +31,10 @@ pub async fn receive(mut receiver: Receiver) {
             (Command::Read, key, LastParam::Read(resp)) => {
                 let result = map.get(&key);
                 match result {
-                    Some(r) => resp.send(Some(r.clone())).unwrap(),
-                    None => resp.send(None).unwrap()
-                }
+                    Some(r) => resp.send(Some(r.clone())),
+                    None => resp.send(None)
+                };
+                ()
             },
             (Command::Write, key, LastParam::Write(val)) => {
                 map.insert(key, val);
@@ -49,14 +45,34 @@ pub async fn receive(mut receiver: Receiver) {
     }
 }
 
+impl TMap {
+    pub fn new() -> TMap {
+        TMap {
+            tables: HashMap::new(),
+        }
+    }
+
+    pub fn create_table(&mut self, name: &str) -> Receiver {
+        let (mut sender, mut receiver) = mpsc::channel::<Channel>(100);
+        self.tables.insert(name.to_string(), sender);
+        receiver
+    }
+
+    pub fn get_sender(&self, name: &str) -> Sender{
+        self.tables.get(name).unwrap().clone()
+    }
+}
+
 pub async fn write(sender: &mut Sender, key: u64, val: u64) {
     if let Err(e) = sender.send((Command::Write, key, LastParam::Write(val))).await {
+        // TODO: handle error
         println!("the error {:}", e);
     };
 }
 
 pub async fn read(sender: &mut Sender, key: u64, resp_sender: oneshot::Sender<Option<u64>>) {
     if let Err(e) = sender.send((Command::Read, key, LastParam::Read(resp_sender))).await {
+      // TODO: handle error
       println!("the error {:}", e);
     }
 }
@@ -67,23 +83,25 @@ mod tests {
 
     #[tokio::test]
     async fn test_basic() {
-        let (mut sender, mut rx) = mpsc::channel::<Channel>(100);
+        let tables = RwLock::new(TMap::new());
+        let mut rx = tables.write().unwrap().create_table("test");
+        tokio::spawn(receive(rx));
 
         tokio::spawn(async move {
-            write(&mut sender, 10 as u64, 11 as u64).await;
+            let sender = tables.read().unwrap().get_sender("test");
+            write(&mut sender.clone(), 10 as u64, 11 as u64).await;
 
-            // read
             let (resp_tx, resp_rx) = oneshot::channel::<Option<u64>>();
-            read(&mut sender, 10 as u64, resp_tx).await;
+            read(&mut sender.clone(), 10 as u64, resp_tx).await;
 
             let v = resp_rx.await.unwrap();
-            println!("got val {:?}", v);
-        });
+            assert_eq!(v, Some(11));
 
-        let r = tokio::spawn(receive(rx));
-        r.await;
+            let (resp_tx, resp_rx) = oneshot::channel::<Option<u64>>();
+            read(&mut sender.clone(), 1123 as u64, resp_tx).await;
 
-        assert_eq!(1, 1);
+            let v = resp_rx.await.unwrap();
+            assert_eq!(v, None);
+        }).await;
     }
-
 }
